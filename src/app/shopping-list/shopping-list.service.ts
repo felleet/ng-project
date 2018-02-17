3d7911640663ea75b94e87221593f2f9b4bca50d
felleet <felleet@gmail.com>
2018-02-17 10:45:12 +0300

    [+] ingredients list update inform through Subject

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
 import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core'
+import {Subject} from "rxjs/Subject";
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   ingredients: Ingredient[] = [
       new Ingredient('Apples', 5),
       new Ingredient('Tomatoes', 10)
@@ -14,7 +15,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
       this.ingredients.push(ingredient);
-      this.ingredientsChanged.emit(this.getIngredients());
+      this.ingredientsChanged.next(this.getIngredients());
   }
 
   addIngredients(ingredients: Ingredient[]){
@@ -23,7 +24,7 @@ export class ShoppingListService {
     }*/
 
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 
 }
