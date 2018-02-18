0378c02d9dec77151c22b39279246861a3ce2677
felleet <felleet@gmail.com>
2018-02-18 12:29:14 +0300

    [+] basic authentication

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import * as firebase from 'firebase'
+
+export class AuthService {
+  token: string;
+
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(
+        error => console.log(error)
+      );
+  }
+
+  signinUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => {
+          firebase.auth().currentUser.getToken()
+            .then(token => this.token = token)
+        }
+      )
+      .catch(
+        error => console.log(error)
+      );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getToken()
+      .then(token => this.token = token);
+    return this.token;
+  }
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,17 +2,23 @@ import {Injectable} from "@angular/core";
 import {Http, Response} from "@angular/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http, private recipeService: RecipeService){}
+  constructor(private http: Http,
+              private recipeService: RecipeService,
+              private authService: AuthService){}
 
   storeRecipes(){
-    return this.http.put('https://ng-recipe-book-8.firebaseio.com/recipes.json', this.recipeService.getRecipes())
+    let token = this.authService.getToken();
+    return this.http.put('https://ng-recipe-book-8.firebaseio.com/recipes.json?auth=' + token,
+      this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book-8.firebaseio.com/recipes.json')
+    let token = this.authService.getToken();
+    this.http.get('https://ng-recipe-book-8.firebaseio.com/recipes.json?auth=' + token)
       .subscribe(
         (response: Response) => {
           let recipes: Recipe[] = response.json();
@@ -20,5 +26,7 @@ export class DataStorageService {
         }
       );
   }
+
+
 }
 
