89f17db2a80ccb0e508e5bfc876e59cf6174b872
felleet <felleet@gmail.com>
2018-02-18 17:37:27 +0300

    [+] migrate to HttpClientModule

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,27 +1,35 @@
 import {Injectable} from "@angular/core";
-import {Http, Response} from "@angular/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
 import {AuthService} from "../auth/auth.service";
+import {HttpClient} from "@angular/common/http";
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService){}
 
   storeRecipes(){
     let token = this.authService.getToken();
-    return this.http.put('https://ng-recipe-book-8.firebaseio.com/recipes.json?auth=' + token,
+    return this.httpClient.put('https://ng-recipe-book-8.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipes());
   }
 
   getRecipes() {
     let token = this.authService.getToken();
-    this.http.get('https://ng-recipe-book-8.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-8.firebaseio.com/recipes.json?auth=' + token)
+      .map((recipes) => {
+        for (let recipe of recipes) {
+          if (recipe['ingredients']) {
+            recipe['ingredients'] = [];
+          }
+        }
+        return recipes;
+      })
       .subscribe(
-        (response: Response) => {
-          let recipes: Recipe[] = response.json();
+        (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes)
         }
       );
